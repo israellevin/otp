f4ce5f406718db757fef7ebe32550c5ad7bfe9f9
Israel <israel@mammon.tiamat>
2014-08-25 21:23:03 +0300

    Some basic JS to read secrets.

--- static/otp.js
+++ static/otp.js
@@ -21,6 +21,64 @@ function log(msg){
     if('string' === typeof msg) $('#log').prepend($('<div>').html(nicedate() + ' ' + msg));
 }
 
+function findroot(leafid){
+    try{return findroot(secrets[leafid].parent_) || leafid;}
+    catch(err){return 'undefined' !== typeof(secrets[leafid]) && leafid;}
+}
+
+function scanforward(rootid){
+    var members = [], unviewed = [];
+    if(secrets[rootid]){
+        members.push(rootid);
+        if('string' !== typeof(secrets[rootid].body)){
+            unviewed.push(rootid);
+        }
+        $.each(secrets[rootid].children, function(idx, childid){
+            var childscan = scanforward(childid);
+            members = members.concat(childscan.members);
+            unviewed = unviewed.concat(childscan.unviewed);
+        });
+    }
+    return {members: members, unviewed: unviewed};
+}
+
+function secretdiv(secret){
+    return $('<div class=secret>').text(secret.name);
+}
+
+$(function(){
+    var keys = $.map(secrets, function(secret, key){
+        return parseInt(key, 10);
+    }).sort();
+    var threadsdiv = $('#threads').extend({
+        threads: [],
+        addthread: function(thread){
+            $('<div>').text(secrets[thread.root].name).appendTo(threadsdiv);
+        }
+    });
+    var thread;
+    while(keys.length > 0){
+        thread = scanforward(keys[0]);
+        thread.root = keys[0];
+        if(thread.unviewed.length > 0){
+            thread.firstunviewed = Math.min.apply(null, thread.unviewed);
+            thread.target = secrets[thread.firstunviewed].parent_;
+        }else{
+            thread.target = Math.max.apply(null, thread.members);
+        }
+        $.each(thread.members, function(idx, key){
+            var pos = keys.indexOf(key);
+            if(pos > -1) keys.splice(pos, 1);
+        });
+        threadsdiv.addthread(thread);
+    }
+    body = $('body');
+    $.each(secrets, function(key, secret){
+        body.append(secretdiv(secret))
+    });
+    $('#nojs').remove();
+});
+
 function jsonp(method, data, callback){
     $.ajax({
         url: method,
@@ -34,8 +92,5 @@ function jsonp(method, data, callback){
             log('Unable to retrieve ' + method);
         }
     });
-};
+}
 
-$(function(){
-    $('#nojs').remove();
-});
